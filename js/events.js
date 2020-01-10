92009d433f0133ce105c5e3efea7f169689b456b
Michelle Ranly <meranly8@gmail.com>
2020-01-10 19:44:54 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,19 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('.tasty').on('load', function() {
+    
+  });
+}
+
+function pressIt() {
+  $('')
+}
 
 $(document).ready(function(){
 
